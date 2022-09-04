f9972392d4fb6a304648fa09ab441dc8a82a3cfb
Egecaney <109510270+Egecaney@users.noreply.github.com>
2022-09-04 14:54:10 +0300

    Update Microcontroller.ino

--- Bitirme Projesi/Microcontroller.ino
+++ Bitirme Projesi/Microcontroller.ino
@@ -5,31 +5,34 @@
 #define YPinNumber 36            // ServoY'nin Dijital Pin Numarası
 #define SonarTrigPinNumber 24    // Ultrasonik Sensör'ün TRIG Pin Numarası
 #define SonarEchoPinNumber 22    // Ultrasonik Sensör'ün ECHO Pin Numarası
-#define SonarMaxDistance 150     // Ultrasonik Sensör'ün Maksimum Uzaklığı (cm cinsinden)
+#define SonarMaxDistance 200     // Ultrasonik Sensör'ün Maksimum Uzaklığı (cm cinsinden)
 #define DirectionPin 3
 #define StepPin 4    
-#define StepMotorSpeed 2
+#define StepMotorSpeed 4
 #define LaserPin 42
 
 VarSpeedServo ServoY;                                  // Y Ekseni için Servo
 Stepper StepMotor = Stepper(200,DirectionPin,StepPin);
+NewPing Sonar(SonarTrigPinNumber,SonarEchoPinNumber,SonarMaxDistance);
 
-int PositionY = 90;                                    // ServoY'nin Pozisyonu
-int PositionofSTEPMotor = 0;                           // Step Motor'un Pozisyonu Radar Ekranı için bilinmeli.
-float SonarDistanceinMeter = 0;                                 // Ultrasonik Sensör'ün Pozisyonu Radar Ekranı için bilinmeli.
+int PositionY = 130;                                    // ServoY'nin Pozisyonu
+int PositionofSTEPMotor = 50;                           // Step Motor'un Pozisyonu Radar Ekranı için bilinmeli.
+int StepFlag = 0;
+int SonarDistance = 0;
 
 char CommandFromPython = ' ';              // PYTHON'dan Gelecek Komut
 
 void ServoYMovement(char RegionY){
       if(RegionY == 'D'){                  // Kamerayı AŞAĞI indirmek için.
-        PositionY += 2;
+        PositionY += 1;
         ServoY.write(PositionY,255,false);
       }
       else if(RegionY == 'U'){             // Kamerayı YUKARI kaldırmak için.
-        PositionY -=2;
+        PositionY -=1;
         ServoY.write(PositionY,255,false);
       }
 }
+
 void StepXMovement(char RegionX){
   if(RegionX == 'L'){
     StepMotor.step(StepMotorSpeed);
@@ -40,74 +43,73 @@ void StepXMovement(char RegionX){
     PositionofSTEPMotor -= 1;
   }
 }
-void FindSonarDistance(void){
-  long int Time;
-  float Distance;
-  digitalWrite(SonarTrigPinNumber,LOW);
-  delayMicroseconds(2);
-  digitalWrite(SonarTrigPinNumber,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(SonarTrigPinNumber,LOW);
-  Time = pulseIn(SonarEchoPinNumber,HIGH);
-  Distance = (Time*0.034)/2 ;
-  if(Distance>0 && Distance < SonarMaxDistance){
-    SonarDistanceinMeter = Distance/100;
-  }
-}
+
 void StepTurningAlways(void){
-  StepMotor.step(StepMotorSpeed);
-  PositionofSTEPMotor += 1;
+  if(!StepFlag){
+    StepMotor.step(StepMotorSpeed);
+    PositionofSTEPMotor += 1;
+    delay(25);
+    if(PositionofSTEPMotor == 100){
+      StepFlag = 1;
+    }
+  }
+  else if(StepFlag){
+    StepMotor.step(-StepMotorSpeed);
+    PositionofSTEPMotor -= 1;
+    delay(25);
+    if(PositionofSTEPMotor == 0){
+      StepFlag = 0;
+    }
+  }
 }
 
 void ReceiveData(void){
-  float SendData;
-  if(PositionofSTEPMotor < 0){
-    PositionofSTEPMotor += 200;
+  SonarDistance = Sonar.ping_cm();
+  if(SonarDistance != 0){
+    Serial.print(PositionofSTEPMotor);
+    Serial.print(",");
+    Serial.print(SonarDistance);
+    Serial.print(".");
+    delay(20);
   }
-  if(PositionofSTEPMotor > 200){
-     PositionofSTEPMotor -= 200;
-  }
-  SendData = PositionofSTEPMotor*10 + SonarDistanceinMeter;
-  Serial.println(SendData,1);
-  SendData = 0;
 }
 
 void setup(){
     ServoY.attach(YPinNumber,400,2600);   // ServoY'yi seçilen dijital pinine bağlanır.
     ServoY.write(PositionY,255,true);     // ServoY'yi başlangıç konumuna getiriyoruz.
     StepMotor.setSpeed(250);              // Step Motor'ın Hızı
-    //pinMode(SonarTrigPinNumber,OUTPUT);
-    //pinMode(SonarEchoPinNumber,INPUT);    
+    pinMode(SonarTrigPinNumber,OUTPUT);
+    pinMode(SonarEchoPinNumber,INPUT);    
     pinMode(LaserPin,OUTPUT);             // Laser Pin'ini OUTPUT olarak tanıtıyoruz.
-    Serial.begin(115200);                 // Ardiuno - PYTHON Baud Rate
+    Serial.begin(9600);                 // Ardiuno - PYTHON Baud Rate
     Serial.setTimeout(.1);                // Ardiuno - PYTHON Timeout
-    }
+}
 
 void loop(){
      if (Serial.available()>0){
       CommandFromPython = Serial.read();
 
       if(CommandFromPython == 'S'){   // Cisim DURMUŞSA
-      //  FindSonarDistance();
-        digitalWrite(LaserPin,LOW);
-        //ReceiveData(); 
+        digitalWrite(LaserPin,HIGH);
+        ReceiveData(); 
       }
       if(CommandFromPython == 'D' || CommandFromPython == 'U'){
-        //ServoYMovement(CommandFromPython);
-        //FindSonarDistance();
+        ServoYMovement(CommandFromPython);
         digitalWrite(LaserPin,HIGH);
-        //ReceiveData();
+        ReceiveData();
       }
       if(CommandFromPython == 'L' || CommandFromPython == 'R'){
         StepXMovement(CommandFromPython);
-        //FindSonarDistance();
-        digitalWrite(LaserPin,LOW);
-        //ReceiveData(); 
+        digitalWrite(LaserPin,HIGH);
+        ReceiveData(); 
       }
       if(CommandFromPython == 'N'){  // Cisim YOKSA                 
-        //StepTurningAlways();
-        //SonarDistanceinMeter = 2;
-        digitalWrite(LaserPin,LOW); 
+        StepTurningAlways();
+        digitalWrite(LaserPin,LOW);
+        Serial.print(PositionofSTEPMotor);
+        Serial.print(",");
+        Serial.print(0);
+        Serial.print(".");
       }
   }
   }
